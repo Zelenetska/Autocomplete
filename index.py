27c72416c6d0863de221cc74ac1379ea4f36ad94
Ira Zelenetska <iryna.zelenetska@gmail.com>
2019-12-18 19:41:13 +0200

    Added timeit module to test the time speed

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-import time
+import timeit
+import sys
 
 import binary_search as Binary
 import trie as Trie
@@ -10,24 +11,25 @@ dictionary_file2 = open("usa.txt", "r")
 # dictionary_file = open("english3.txt", "r")
 # dictionary_file2 = open("english3.txt", "r")
 
+root = Trie.TrieNode('*')
+for word in dictionary_file:
+    word = word.strip()
+    Trie.add(root, word)
+
+dictionary_arr = dictionary_file2.read().splitlines()
+dictionary_arr.sort()
+
+
+def trie_test():
+    return Trie.find_prefix(root, 'appl')
+
+
+def binary_test():
+    return Binary.binary_search(dictionary_arr, 'appl')
+
+
 if __name__ == "__main__":
-    root = Trie.TrieNode('*')
-
-    # Approximate search time - 5 - 6 ms
-    # Search time depends on the length of the prefix and number of possible endings
-    for word in dictionary_file:
-        word = word.strip()
-        Trie.add(root, word)
-
-    start_time = time.time()
-    print(Trie.find_prefix(root, 'trick'))
-    end_time = time.time() - start_time
-    print(end_time)
-
-    # Approximate search time - 4 - 5 ms on both files
-    dictionary_arr = dictionary_file2.read().splitlines()
-    dictionary_arr.sort()
-    start_time = time.time()
-    print(Binary.binary_search(dictionary_arr, 'trick'))
-    end_time = time.time() - start_time
-    print(end_time)
+    trie_times = timeit.repeat("trie_test()", setup="from __main__ import trie_test", repeat=5, number=1000000)
+    binary_times = timeit.repeat("binary_test()", setup="from __main__ import binary_test", repeat=5, number=1000000)
+    print('Trie search time: {}'.format(trie_times))
+    print('Binary search time: {}'.format(binary_times))
